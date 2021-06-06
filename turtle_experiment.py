2efd6e55b3214521983f54d5b2b9bb0bede41bdc
GHOSH <RUPSHA.GHOSH@finastra.com>
2021-06-06 14:35:01 +0530

    adding the first experiment file

---
+++ turtle_experiment.py
@@ -0,0 +1,22 @@
+import turtle
+
+# Rups = turtle.Turtle() <---- can be used if we want to create an object of the class before using it
+turtle.color('red')
+turtle.pensize('6')
+turtle.shape('arrow')
+
+turtle.forward(100)
+turtle.penup()
+turtle.left(80)
+turtle.pendown()
+turtle.left(45)
+turtle.forward(100)
+turtle.right(270)
+turtle.forward(500)
+turtle.color('green')
+turtle.left(45)
+turtle.forward(40)
+
+turtle.done()
+
+
